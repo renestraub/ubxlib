deec004aa89ce0053fb3f3d2812f8abcaeb311a4
Rene Straub <rene@see5.ch>
2020-04-28 18:50:30 +0200

    Add ESF status example

---
+++ examples/show_esf_status.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Shows status of extended sensor fusion
+"""
+import logging
+import time
+
+from ubxlib.server import GnssUBlox
+from ubxlib.ubx_esf_status import UbxEsfStatusPoll, UbxEsfStatus
+
+
+FORMAT = '%(asctime)-15s %(levelname)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('gnss_tool')
+logger.setLevel(logging.INFO)
+# logger.setLevel(logging.DEBUG)
+
+
+# Create UBX library
+# Note: tty is only used to address modem in gpsd.
+#       the library does not use the tty directly
+ubx = GnssUBlox('/dev/ttyS3')
+ubx.setup()
+
+# Register the frame types we use
+ubx.register_frame(UbxEsfStatus)
+
+# Get state and print in full once
+res = ubx.poll(UbxEsfStatusPoll())
+print(f'Received answer from modem\n{res}')
+
+print('Checking state, press CTRL+C to abort')
+
+try:
+    esf_status_request = UbxEsfStatusPoll()
+    while True:
+        res = ubx.poll(esf_status_request)
+        print(f'GNSS time of week {res.f.iTow}')
+        print(f'  {res.get("fusionMode")} (value: {res.get("fusionMode").value})')
+        print(f'  {res.get("initStatus1")}')
+        print(f'  {res.get("initStatus2")}')
+
+        time.sleep(1.0)
+
+except KeyboardInterrupt:
+    print()
+    print('Done')
+
+ubx.cleanup()
