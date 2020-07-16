6dd2a2fb1194cef2dff3103fed3e6f893bddd0e1
Rene Straub <rene@see5.ch>
2020-07-16 18:27:34 +0200

    Add UBX-CFG-RATE

---
+++ examples/set_nav_rate.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Sample code to change navigation output rate to 2 Hz
+
+Run as module from project root:
+python3 -m examples.set_nav_rate
+"""
+import logging
+
+from ubxlib.server import GnssUBlox
+from ubxlib.ubx_cfg_rate import UbxCfgRatePoll, UbxCfgRate
+
+
+FORMAT = '%(asctime)-15s %(levelname)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('gnss_tool')
+# logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
+rate = int(input('Please enter desired rate in Hz (1..5): '))
+assert 1 <= rate <= 5
+
+# Create UBX library
+ubx = GnssUBlox()
+ubx.setup()
+
+# Register the frame types we use
+ubx.register_frame(UbxCfgRate)
+
+# Query modem for current rate settings
+poll_rate_cfg = UbxCfgRatePoll()
+res = ubx.poll(poll_rate_cfg)
+print(res)
+
+# Now change to desired navigation output rate
+res.set_rate_in_hz(rate)
+
+# Send command to modem, result is ack_nak from modem
+ack_nak = ubx.set(res)
+print(ack_nak)      # Just print result, could also check for ACK-ACK CID
+
+ubx.cleanup()
---
+++ ubxlib/ubx_cfg_rate.py
@@ -0,0 +1,29 @@
+from ubxlib.frame import UbxFrame, UbxCID
+from ubxlib.types import U2
+
+
+class UbxCfgRate_(UbxFrame):
+    CID = UbxCID(0x06, 0x08)
+    NAME = 'UBX-CFG-RATE'
+
+
+class UbxCfgRatePoll(UbxCfgRate_):
+    NAME = UbxCfgRate_.NAME + '-POLL'
+
+    def __init__(self):
+        super().__init__()
+
+
+class UbxCfgRate(UbxCfgRate_):
+    def __init__(self):
+        super().__init__()
+
+        self.f.add(U2('measRate'))  # Time elapsed between two measuremnts in ms
+        self.f.add(U2('navRate'))   # Number of measurements for NAV solution
+        self.f.add(U2('timeRef'))
+
+    def set_rate_in_hz(self, rate):
+        # should not go beyond 10 Hz
+        assert 1 <= rate <= 10
+        self.f.measRate = int(1000 / rate)
+        self.f.navRate = 1
