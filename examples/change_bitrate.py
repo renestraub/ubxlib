6e91a41f3385fe29e616f560b90b3fd034b642a9
Rene Straub <rene@see5.ch>
2020-08-20 18:37:43 +0200

    Add tty backend
    tty backend allow to talk to GNSS module without
    gpsd. The use case was to detect and change the
    receiver baudrate, so that gpsd can be started
    with a well known bitrate (no auto detection
    required)

---
+++ examples/change_bitrate.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+Gets current modem bitrate and changes it to 115'200
+"""
+import logging
+
+from ubxlib.server_tty import GnssUBloxBitrate
+from ubxlib.server_tty import GnssUBlox
+from ubxlib.ubx_cfg_prt import UbxCfgPrtPoll, UbxCfgPrtUart
+
+
+TTY = '/dev/ttyS3'
+
+FORMAT = '%(asctime)-15s %(levelname)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('gnss_tool')
+# logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.INFO)
+logger.setLevel(logging.WARNING)
+
+
+# Check current bitrate of modem
+ubx_bitrate = GnssUBloxBitrate(TTY)
+
+print('Determining current bitrate')
+bitrate = ubx_bitrate.determine()
+
+print(f'Current GNSS receiver bitrate is {bitrate} bps')
+assert bitrate
+
+
+# Create UBX library
+# Note: tty and baudrate must match current receiver configuration
+ubx = GnssUBlox(TTY, bitrate)
+ubx.setup()
+
+# Register the frame types we use
+ubx.register_frame(UbxCfgPrtUart)
+
+# Poll current tty port settings from modem
+poll_cfg = UbxCfgPrtPoll()
+poll_cfg.f.PortId = UbxCfgPrtPoll.PORTID_Uart
+
+port_cfg = ubx.poll(poll_cfg)
+if port_cfg:
+    print(f'Current port config is\n{port_cfg}')
+    print(f'Current modem bitrate is {port_cfg.f.baudRate} bps')
+    if port_cfg.f.baudRate == 115200:
+        print('Changing bitrate to 9600')
+        port_cfg.f.baudRate = 9600
+    else:
+        print('Changing bitrate to 115200')
+        port_cfg.f.baudRate = 115200
+
+    # Note: usually we would use ubx.set() to send the request and wait for an ack
+    # Unfortunately the modem changes the bitrate immediately so the ACK is not
+    # received.
+    port_cfg.pack()
+    ubx.send(port_cfg)
+
+    print('done, please re-run to check effect')
+else:
+    # No answer...
+    pass
+
+ubx.cleanup()
---
+++ ubxlib/server_tty.py
@@ -0,0 +1,260 @@
+import binascii
+import logging
+import queue
+import sys
+import threading
+import time
+
+from serial import Serial
+from serial.serialutil import SerialException
+
+from ubxlib.frame_factory import FrameFactory
+from ubxlib.frame import UbxFrame, UbxCID
+from ubxlib.ubx_ack import UbxAckAck
+from ubxlib.parser import UbxParser
+
+
+logger = logging.getLogger('gnss_tool')
+
+# TODO: Factor out code shared with server.py and create baseclass
+class GnssUBlox(threading.Thread):
+    def __init__(self, device_name=None, baudrate=115200):
+        super().__init__()
+
+        self.device_name = device_name
+        self.baudrate = baudrate
+        self.enabled = False
+
+        self.serial_port = None
+        self.response_queue = queue.Queue()
+        self.parser = UbxParser(self.response_queue)
+        self.thread_ready_event = threading.Event()
+        self.thread_stop_event = threading.Event()
+
+        self.frame_factory = FrameFactory.getInstance()
+        self.wait_cid = None
+        self.gpsd_errors = 0
+        self.cid_error = UbxCID(0x00, 0x01)
+
+    def setup(self):
+        # Register ACK-ACK frame, as it's used internally by this module
+        self.frame_factory.register(UbxAckAck)
+
+        # Start worker thread in daemon mode, will invoke run() method
+        self.daemon = True
+        self.start()
+
+        # Wait for worker thread to become ready.
+        # Without this wait we might send the command before the thread can
+        # handle the response.
+        logger.info('waiting for receive thread to become active')
+        ready = self.thread_ready_event.wait(timeout=1.0)
+        if not ready:
+            logger.error('timeout while connecting to device')
+
+        return ready
+
+    def cleanup(self):
+        logger.info('requesting thread to stop')
+        self.thread_stop_event.set()
+
+        # Wait until thread ended
+        self.join(timeout=1.0)
+        logger.info('thread stopped')
+
+    def register_frame(self, frame_type):
+        self.frame_factory.register(frame_type)
+
+    def poll(self, message):
+        """
+        Poll a receiver status
+
+        - sends the specified poll message
+        - waits for receiver message with same class/id as poll message
+        """
+        assert isinstance(message, UbxFrame)
+
+        self.expect(message.CID)
+        self.send(message)
+        res = self.wait()
+        return res
+
+    def set(self, message):
+        """
+        Send a set message to modem
+
+        - creates bytes representation of set frame
+        - send set message to modem
+        - waits for ACK
+        """
+        assert isinstance(message, UbxFrame)
+
+        self.expect(UbxAckAck.CID)
+        message.pack()
+        self.send(message)
+        res = self.wait()
+
+        return res
+
+    def expect(self, cid):
+        """
+        Define message message to wait for
+        """
+        logger.debug(f'expecting {cid}')
+        self.wait_cid = cid
+        self.parser.set_filter(cid)
+
+    def send(self, ubx_message):
+        assert self.enabled
+
+        logger.debug(f'sending {ubx_message}')
+
+        # TODO: first call .pack()
+        msg_in_binary = ubx_message.to_bytes()
+        # logger.debug(f'sending {msg_in_binary}')
+
+        # Send frame to modem tty
+        bytes_sent = self.serial_port.write(msg_in_binary)
+        logger.debug(f"sent {bytes_sent} bytes")
+
+        if bytes_sent != len(msg_in_binary):
+            self.gpsd_errors += 1
+            logger.warning(f'command not accepted by tty, {self.gpsd_errors} error(s)')
+
+            # Report error to waiting client, so it does not block
+            # Use custom CID not used by u-blox, if there was someting
+            # like UBX-ACK-TIMEOUT we would use that.
+            # TODO: Consider UBX-ACK-NAK ..
+            self.response_queue.put((self.cid_error, None))
+        else:
+            self.gpsd_errors = 0
+
+    def wait(self, timeout=3.0):
+        logger.debug(f'waiting {timeout}s for reponse from listener thread')
+
+        # TODO: Timeout loop required if we have queue.get() with timeout?
+        time_end = time.time() + timeout
+        while time.time() < time_end:
+            try:
+                cid, data = self.response_queue.get(True, timeout)
+                logger.debug(f'got response {cid}')
+
+                if cid == self.cid_error:
+                    logger.warning('error response, no frame available')
+
+                    self.parser.clear_filter()
+                    return None
+
+                # TODO; Required if parser already filters for us?
+                elif cid == self.wait_cid:
+                    logger.debug(f'received expected frame {cid}')
+
+                    ff = FrameFactory.getInstance()
+                    try:
+                        frame = ff.build_with_data(cid, data)
+
+                    except KeyError:
+                        # If we can't parse the frame, return as is
+                        logger.debug(f'default: {binascii.hexlify(data)}')
+                        frame = UbxFrame()
+
+                    self.parser.clear_filter()
+                    return frame
+
+            except queue.Empty:
+                logger.warning('timeout...')
+
+    def run(self):
+        """
+        Thread running method
+
+        - receives raw data from tty
+        - parses ubx frames, decodes them
+        - if a frame is received it is put in the receive queue
+        """
+        try:
+            logger.info(f'connecting to {self.device_name} at {self.baudrate} bps')
+            self.serial_port = Serial(self.device_name, timeout=0.1, baudrate=self.baudrate)
+
+            logger.debug('starting listener on tty')
+            self.thread_ready_event.set()
+            self._main_loop()
+
+            logger.debug('receiver done')
+
+        except SerialException as msg:
+            logger.error(msg)
+
+    def _main_loop(self):
+        self.enabled = True
+        while not self.thread_stop_event.is_set():
+            try:
+                data = self.serial_port.read(1024)
+                if data:
+                    self.parser.process(data)
+            except Serial.timeout:
+                pass
+
+        self.enabled = False
+
+
+class GnssUBloxBitrate:
+    def __init__(self, device_name=None):
+        super().__init__()
+
+        self.device_name = device_name
+        self.baudrate = None
+
+    def determine(self):
+        """
+        Determine current baudrate of GNSS modem
+
+        Theory of operation:
+        - set well known bitrate and check whether data can be received
+        - this assumes the modem is transmitting ubx or NMEA frames
+        """
+        bitrates_to_check = [115200, 9600]
+        self.baudrate = None
+
+        for b in bitrates_to_check:
+            logger.info(f'checking bitrate {b}')
+            ser = Serial(self.device_name, timeout=0.1, baudrate=b)
+            ser.reset_input_buffer()
+
+            data_raw = bytes()
+            data_nmea = ''
+
+            # Without further configuration a set of data is sent once per second.
+            # 15 loops with a 0.1 sec read timeout is therefore enough for a decision
+            for _ in range(15):
+                data = ser.read(1024)
+                data_raw += data
+
+                # Disregard conversion errors when binary data or garbage is received
+                nmea = ''
+                try:
+                    nmea = data_raw.decode()
+                    data_nmea += nmea
+                except UnicodeError:
+                    # Unicode conversion errors are expected here
+                    pass
+
+                logger.debug(f'bin: {binascii.hexlify(data_raw)}, ascii: {nmea}')
+
+                # Check for NMEA frames (ASCII)
+                if 'GGA' in data_nmea:
+                    logger.info(f'nmea sentence detected at {b} bps')
+                    self.baudrate = b
+                    break
+                # Check for ubx frames
+                elif bytes.fromhex('B562') in data_raw:
+                    logger.info(f'ubx frame detected at {b} bps')
+                    self.baudrate = b
+                    break
+
+            if self.baudrate:
+                logger.info(f"current bitrate of GNSS is {b}")
+                return b
+
+        # If we get here, we couldn't find the bitrate
+        return None
