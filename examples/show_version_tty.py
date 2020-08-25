715a63bdc6e66c1f3936e402ab77156e61cc66b8
Rene Straub <rene@see5.ch>
2020-08-25 20:32:13 +0200

    Add example for use of serial_tty backend

---
+++ examples/show_version_tty.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Simple demonstrator that gets modem version
+"""
+import logging
+
+from ubxlib.server_tty import GnssUBlox
+from ubxlib.ubx_mon_ver import UbxMonVerPoll, UbxMonVer
+
+
+FORMAT = '%(asctime)-15s %(levelname)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('gnss_tool')
+# logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
+
+# Create UBX library
+# Note: tty and baudrate must match current receiver configuration
+ubx = GnssUBlox('/dev/ttyS3', 115200)
+ubx.setup()
+
+# Register the frame types we use
+ubx.register_frame(UbxMonVer)
+
+# Poll version from modem
+poll_version = UbxMonVerPoll()
+res = ubx.poll(poll_version)
+
+# Simple print of received answer frame
+print(f'Received answer from modem\n{res}')
+
+# Can also access fields of UbxMonVer via .f member
+print()
+print(f'SW Version: {res.f.swVersion}')
+print(f'HW Version: {res.f.hwVersion}')
+
+ubx.cleanup()
