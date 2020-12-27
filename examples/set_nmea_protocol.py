dba1e44defaf4a88f97d80c3a51a7e051497d053
Rene Straub <rene@see5.ch>
2020-12-27 19:28:02 +0100

    Cleanup NMEA protocol example

--- examples/set_nmea_protocol.py
+++ examples/set_nmea_protocol.py
@@ -8,18 +8,20 @@ python3 -m examples.set_nmea_protocol
 import logging
 
 from ubxlib.server import GnssUBlox
+# from ubxlib.server_tty import GnssUBlox     # TTY direct backend
 from ubxlib.ubx_cfg_nmea import UbxCfgNmeaPoll, UbxCfgNmea
 
 
 FORMAT = '%(asctime)-15s %(levelname)-8s %(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('ubxlib')
-logger.setLevel(logging.INFO)
-# logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 # Create UBX library
 ubx = GnssUBlox()
+# ubx = GnssUBlox('/dev/gnss0', 115200)
 ubx.setup()
 
 # Register the frame types we use
@@ -28,17 +30,19 @@ ubx.register_frame(UbxCfgNmea)
 # Query modem for current protocol settings
 poll_nmea_cfg = UbxCfgNmeaPoll()
 res = ubx.poll(poll_nmea_cfg)
+if res:
+    proto_ver = res.f.nmeaVersion // 16     # Protovol is encoded in high/low nibble
+    proto_rev = res.f.nmeaVersion % 16      # e.g. 0x40 = 4.0
+    print(f'current NMEA protocol version is {proto_ver}.{proto_rev}')
 
-proto_ver = res.f.nmeaVersion // 16     # Protovol is encoded in high/low nibble
-proto_rev = res.f.nmeaVersion % 16      # e.g. 0x40 = 4.0
-print(f'current NMEA protocol version is {proto_ver}.{proto_rev}')
+    # Now change to something different
+    if res.f.nmeaVersion == 0x40:
+        res.f.nmeaVersion = 0x41
+    else:
+        res.f.nmeaVersion = 0x40
 
-# Now change to something different
-# res.f.nmeaVersion = 0x40
-res.f.nmeaVersion = 0x41
-
-# Send command to modem, result is ack_nak from modem
-ack_nak = ubx.set(res)
-print(ack_nak)      # Just print result, could also check for ACK-ACK CID
+        # Send command to modem, result is ack_nak from modem
+    ack_nak = ubx.set(res)
+    print(ack_nak)      # Just print result, could also check for ACK-ACK CID
 
 ubx.cleanup()
