c8ad20e7ac4a48633f66ffbd1448c9d9da5bf629
Rene Straub <rene@see5.ch>
2021-01-07 20:38:23 +0100

    Cleanup nmea parser test suite

--- tests/test_parser_nmea.py
+++ tests/test_parser_nmea.py
@@ -15,21 +15,21 @@ class TestParserNmea:
         assert NmeaParser._to_bin('$') == -1
 
     def test_ok(self):
-        data = bytes("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A".encode())
+        data = "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A".encode()
         uut = NmeaParser()
         assert uut.frames_rx == 0
         uut.process(data)
         assert uut.frames_rx == 1
 
     def test_wrong_checksum(self):
-        data = bytes("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6B".encode())
+        data = "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6B".encode()
         uut = NmeaParser()
         uut.process(data)
         assert uut.frames_rx == 0
 
     def test_checksum_missing(self):
-        data_fail = bytes("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W".encode())
-        data = bytes("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A".encode())
+        data_fail = "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W".encode()
+        data = "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A".encode()
         uut = NmeaParser()
         uut.process(data_fail)
         assert uut.frames_rx == 0
@@ -38,12 +38,12 @@ class TestParserNmea:
         assert uut.frames_rx == 1
 
     def test_checksum_invalid_chars(self):
-        data1 = bytes("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*xx".encode())
+        data1 = "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*xx".encode()
         uut = NmeaParser()
         uut.process(data1)
         assert uut.frames_rx == 0
 
-        data2 = bytes("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*0g".encode())
+        data2 = "$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*0g".encode()
         uut = NmeaParser()
         uut.process(data2)
         assert uut.frames_rx == 0
