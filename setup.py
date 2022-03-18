37f2b9d0af07183db01376c668efc36cc1c65c02
renestraub <rene@see5.ch>
2022-03-18 20:42:33 +0100

    Exclude tests from install package (#55)
    Resolves #53

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/renestraub/ubxlib",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=("tests",)),
     classifiers=[
         'Programming Language :: Python :: 3.7',
         "License :: OSI Approved :: MIT License",
