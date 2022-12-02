cefeef2eead76e339b1e9dc2e03be45f271e281e
Rene Straub <rene.straub@netmodule.com>
2022-12-02 14:25:05 +0100

    Update NEO-M9 examples

--- examples/set_nav_rate_m9.py
+++ examples/set_nav_rate_m9.py
@@ -9,6 +9,7 @@ import logging
 
 # from ubxlib.server import GnssUBlox   # gpsd socket
 from ubxlib.server_tty import GnssUBlox     # TTY direct backend
+from ubxlib.cfgkeys import UbxKeyId
 from ubxlib.types import CfgKeyData
 from ubxlib.ubx_cfg_valset import UbxCfgValSetAction
 
@@ -22,6 +23,7 @@ logger.setLevel(logging.INFO)
 
 rate = int(input('Please enter desired rate in Hz (1..5): '))
 assert 1 <= rate <= 5
+period_in_ms = int(1000/rate)
 
 # Create UBX library
 # ubx = GnssUBlox()
@@ -31,13 +33,14 @@ ubx.setup()
 # Create measurement rate config key
 # Value = Nominal time between GNSS measurements in [ms], e.g. 100 ms results in
 # 10 Hz measurement rate.
-cfgkey_rate_measure = CfgKeyData.from_u16('data0', 0x21, 0x001, int(1000/rate))
+cfgkey_rate_measure = CfgKeyData.from_key(UbxKeyId.CFG_RATE_MEAS, period_in_ms)
 
-# Set desired rate. Note: Compared to M8 receivers no readback - modify is needed,
-# just set configuration value
+# Set desired rate. Note: Compared to M8 receivers no readback - modify of whole
+# RATE settings is needed, just set configuration value CFG_RATE_MEAS.
 cfg_setval = UbxCfgValSetAction([cfgkey_rate_measure])
 
 ack_nak = ubx.set(cfg_setval)
 print(ack_nak)      # Just print result, no further check
+
 
 ubx.cleanup()
